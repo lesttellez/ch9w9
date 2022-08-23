4c00b8577a7460a0a055e6f89d498cb96ba112bd
Celeste T <celestet@Celestes-MacBook-Air.local>
2022-08-22 19:54:18 -0700

    fixing renderlicese

--- Develop/utils/generatemarkdown.js
+++ Develop/utils/generatemarkdown.js
@@ -1,10 +1,37 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let badge = "";
+  
+    if(license != "None") {
+      badge = "![License Badge](https://shields.io/badge/license-" + license + "-green)";
+    }
+  
+    return badge;
+  }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseLink;
+  
+    switch(license) {
+      case "MIT":
+        licenseLink = "https://mit-license.org/";
+        break;
+      case "Apache":
+        licenseLink = "https://www.apache.org/licenses/LICENSE-2.0.html";
+        break;
+      case "GPL":
+        licenseLink = "https://www.gnu.org/licenses/gpl-3.0.en.html";
+        break;
+      default:
+        licenseLink = "";
+        break;
+    }
+    return licenseLink;
+  
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
