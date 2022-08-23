88c1a027c00fdb082ae9be8cd2cc3925f366db52
Celeste T <celestet@Celestes-MacBook-Air.local>
2022-08-22 19:58:07 -0700

    finish rendering code

--- Develop/utils/generatemarkdown.js
+++ Develop/utils/generatemarkdown.js
@@ -35,12 +35,51 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseSect = "";
+  
+    if (license != "None") {
+      licenseSect += "## License\n"
+      licenseSect += "Please see " + renderLicenseLink(license) + " to get detailed information for this license\n";
+    }
+  
+    return licenseSect;
+  }
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-`;
+  const sections = ["Description", "Installation", "Usage", "Contributors", "Tests", "License", "Questions"];
+    let markdown = "# " + data.title + "\n";
+    markdown += renderLicenseSection(data.license) + "\n";
+    markdown += renderLicenseBadge(data.license) + "\n";
+    markdown += "## Table of Contents\n";
+    for (let i=0; i<sections.length; i++) {
+      if (! (sections[i] === "License" && data.license === "None")) {
+        markdown += i+1 + ". [" + sections[i] + "](#" + sections[i][0].toLowerCase() + sections[i].substring(1) + ")\n";
+      }
+    }
+    markdown += "\n";
+  
+    markdown += "## " + sections[0] + "\n";
+    markdown += data.description + "\n";
+  
+    markdown += "## " + sections[1] + "\n";
+    markdown += data.install + "\n";
+  
+    markdown += "## " + sections[2] + "\n";
+    markdown += data.usage + "\n";
+  
+    markdown += "## " + sections[3] + "\n";
+    markdown += data.guidelines + "\n";
+  
+    markdown += "## " + sections[4] + "\n";
+    markdown += data.tests + "\n";
+  
+    markdown += "## " + sections[6] + "\n";
+    markdown += "Click [HERE](https://github.com/" + data.username + ") to see my Github\n";
+    markdown += "My email is " + data.email + " for any additional questions.\n"
+  return markdown;
 }
 
 module.exports = generateMarkdown;
