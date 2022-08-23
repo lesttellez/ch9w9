5ac3e9aef09700a14d5dda0291fa3240a5d5c159
Celeste T <celestet@Celestes-MacBook-Air.local>
2022-08-22 19:48:15 -0700

    adding to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -54,13 +54,19 @@ const questions = [
     },
 ];
 
-];
-
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    const filename = "README.md";
+    fs.writeFile(filename, data, (err) =>
+        err ? console.log(err) : console.log(filename + " created!")
+        );
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then (answers => writeToFile(generateMarkdown(answers)))
+};
 
 // Function call to initialize app
 init();
